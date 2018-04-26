21d77e1df2db07b98f7b3b99c45c532ef169794f
swarajk7 <swarajk7@gmail.com>
2018-04-26 00:18:40 -0500

    Added code for generating input batches

--- helper.py
+++ helper.py
@@ -1,3 +1,7 @@
+import numpy as np
+import random
+
+
 class WordVoab():
     def __init__(self):
         self.word2ind = dict()
@@ -29,3 +33,60 @@ class WordVoab():
 
     def decode(self, ind):
         return self.ind2word[ind]
+
+
+class BatchBuilder():
+    def __init__(self, v):
+        if not isinstance(v, WordVoab):
+            raise Exception('Please initialize with a vocabulary!!')
+        self.vocabulary = v
+
+    def pad_batch(self, batch):
+        maximumEncLen = -1
+        maximumDecLen = -1
+        # find the maximum encoder and decoder length in current input batch and use it later to pad the input and make it to same size.
+        for b in range(len(batch)):
+            maximumEncLen = max(maximumEncLen, len(batch[b][0]))
+            maximumDecLen = max(maximumDecLen, len(batch[b][1]))
+
+        v = self.vocabulary
+        fixed_batch = []
+
+        # iterate through the batch and append needed paddings.
+        for b in range(len(batch)):
+            needed_encoder_paddings = maximumEncLen - len(batch[b][0])
+            needed_decoder_paddings = maximumDecLen - len(batch[b][1])
+            encoder_ips = [v.encode(v.pad)] * \
+                needed_encoder_paddings + batch[b][0]
+            decoder_ips = [v.encode(v.go)] + batch[b][1] + \
+                [v.encode(v.pad)] * needed_decoder_paddings
+            decoder_ops = batch[b][1] + \
+                [v.encode(v.eos)] + [v.encode(v.pad)] * needed_decoder_paddings
+            fixed_batch.append([encoder_ips, decoder_ips, decoder_ops])
+        return fixed_batch
+
+    def generate_batches(self, training_samples, batch_size=3):
+        random.shuffle(training_samples)
+        start_index = 0
+        batches = []
+        while start_index < len(training_samples):
+            batch = training_samples[start_index:min(
+                start_index + batch_size, len(training_samples))]
+            batches.append(self.pad_batch(batch))
+            start_index += batch_size
+        return batches
+
+'''
+if __name__ == '__main__':
+    training_samples = []
+    for _ in range(10):
+        Q = list(np.random.randint(0, 5, size=np.random.randint(3, 6)))
+        A = list(np.random.randint(0, 5, size=np.random.randint(3, 6)))
+        training_samples.append([Q, A])
+    batcher = BatchBuilder(WordVoab())
+    bs = batcher.generate_batches(training_samples)
+    for b in bs:
+        for b_ in b:
+            print(b_)
+        print('***')
+'''
